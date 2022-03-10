fff153c37dc384bac70dd94bab43cce2ff1a52f9
Rafael Pontes <81950540+leafarpontes@users.noreply.github.com>
2022-03-10 17:37:23 -0300

    Adicionado hash, alterado nome de variáveis
    Adicionado hash com o tipo de receita.
    Nome de variáveis atualizadas seguindo padrão snake_case, de acordo com The Ruby Style Guide.

--- cookbook.rb
+++ cookbook.rb
@@ -2,26 +2,33 @@ puts "Bem-vindo ao CookBook, sua rede social de receitas!"
 
 receitas = []
 
+
 puts "[1] Cadastrar uma receita"
 puts "[2] Ver todas as receitas"
 puts "[3] Sair"
 
 print "Escolha uma opção: "
 
-menuOption = gets.to_i
+menu_option = gets.to_i
 
-while menuOption != 3 do
-    if menuOption == 1
+while menu_option != 3 do
+    if menu_option == 1
 
         print "Digite o nome da receita: "
         nomeReceita = gets.chomp
-        receitas << nomeReceita
+        print "Digite o tipo da receita: "
+        tipoReceita = gets.chomp
 
-    elsif menuOption == 2
+        receitas << { nome: nomeReceita, tipo: tipoReceita }
+
+        puts
+        puts "Receita #{nomeReceita} cadastrada com sucesso!"
+
+    elsif menu_option == 2
 
         puts "######## Receitas Cadastradas ########"
         receitas.each do |receita|
-            puts receita
+            puts "#{receita[:nome]} - #{receita[:tipo]}"
         end
         puts "######################################"
 
@@ -33,7 +40,7 @@ while menuOption != 3 do
     puts "[2] Ver todas as receitas"
     puts "[3] Sair"
     print "Escolha uma opção: "
-    menuOption = gets.to_i
+    menu_option = gets.to_i
 end
 
 puts "Obrigado por usar o Cookbook, até logo"
